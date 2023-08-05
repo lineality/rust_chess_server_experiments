43244533f3c775e560b5747a75ffddff205f323d
lineality <lineality@gmail.com>
2023-08-05 10:55:09 -0400

    black and white svg

--- tinyhttp_chess/src/main.rs
+++ tinyhttp_chess/src/main.rs
@@ -1672,6 +1672,145 @@ fn str_filter_alternating(input_string: &str) -> String {
 fn generate_white_oriented_chessboard(
     chessboard: &[[char; 8]; 8], 
     from: Option<(usize, usize)>, 
+        to: Option<(usize, usize)>
+    ) -> Document {
+
+    let mut doc = Document::new()
+        .set("width", "500")  // Adjusting the width to account for labels
+        .set("height", "500")  // Adjusting the height to account for labels
+        .set("viewBox", (0, 0, 500, 500))
+        .set("style", "background-color: #000;");  // Set background to black
+
+    // Define labels
+    let column_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    let row_labels = ['8', '7', '6', '5', '4', '3', '2', '1'];  // Chessboard starts with 8 from the top
+
+    // Add column labels
+    for (idx, label) in column_labels.iter().enumerate() {
+        let label_text = Text::new()
+            .set("x", 50 + idx * 50 + 25)  // Adjusting the x-coordinate to account for labels
+            .set("y", 472)  // Positioning the label slightly above the bottom edge
+            .set("text-anchor", "middle")
+            .set("font-size", 20)
+            .set("fill", "#757575")  // Set text color to white
+            .add(svg::node::Text::new(label.to_string()));
+        doc = doc.add(label_text);
+    }
+
+    // Add row labels
+    for (idx, label) in row_labels.iter().enumerate() {
+        let label_text = Text::new()
+            .set("x", 32)  // Positioning the label slightly to the right of the left edge
+            .set("y", 50 + idx * 50 + 35)  // Adjusting the y-coordinate to account for labels
+            .set("text-anchor", "middle")
+            .set("font-size", 20)
+            .set("fill", "#757575")  // Set text color to white
+            .add(svg::node::Text::new(label.to_string()));
+        doc = doc.add(label_text);
+    }
+
+    for (row, row_pieces) in chessboard.iter().enumerate() {
+        for (col, &piece) in row_pieces.iter().enumerate() {
+            let x = 50 + col * 50;  // Adjusting the x-coordinate to account for labels
+            let y = 50 + row * 50;  // Adjusting the y-coordinate to account for labels
+
+            let square_color = if (row + col) % 2 == 0 {
+                "#ccc"
+            } else {
+                "#666"
+            };
+
+            let square = Rectangle::new()
+                .set("x", x)
+                .set("y", y)
+                .set("width", 50)
+                .set("height", 50)
+                .set("fill", square_color);
+
+            doc = doc.add(square);
+
+            if piece != ' ' {
+
+
+                // setting from an to color
+                if let Some(from_coords) = from {
+                    let (row, col) = from_coords;
+                    let x = 50 + col * 50;
+                    let y = 50 + row * 50;
+                
+                    let highlight = Rectangle::new()
+                        .set("x", x)
+                        .set("y", y)
+                        .set("width", 50)
+                        .set("height", 50)
+                        .set("fill", "none") // Transparent fill
+                        .set("stroke", "#3189D9")
+                        .set("stroke-width", 3);
+                
+                    doc = doc.add(highlight);
+                }
+                
+                if let Some(to_coords) = to {
+                    let (row, col) = to_coords;
+                    let x = 50 + col * 50;
+                    let y = 50 + row * 50;
+                
+                    let highlight = Rectangle::new()
+                        .set("x", x)
+                        .set("y", y)
+                        .set("width", 50)
+                        .set("height", 50)
+                        .set("fill", "none") // Transparent fill
+                        .set("stroke", "#3189D9")
+                        .set("stroke-width", 3);
+                
+                    doc = doc.add(highlight);
+                }
+
+
+                // map character to piece name and background
+                let (piece_name, background) = match piece {
+                    'p' => ("black_pawn", if square_color == "#666" {"darksquare"} else {"lightsquare"}),
+                    'r' => ("black_rook", if square_color == "#666" {"darksquare"} else {"lightsquare"}),
+                    'n' => ("black_night", if square_color == "#666" {"darksquare"} else {"lightsquare"}),
+                    'b' => ("black_bishop", if square_color == "#666" {"darksquare"} else {"lightsquare"}),
+                    'q' => ("black_queen", if square_color == "#666" {"darksquare"} else {"lightsquare"}),
+                    'k' => ("black_king", if square_color == "#666" {"darksquare"} else {"lightsquare"}),
+                    'P' => ("white_pawn", if square_color == "#666" {"darksquare"} else {"lightsquare"}),
+                    'R' => ("white_rook", if square_color == "#666" {"darksquare"} else {"lightsquare"}),
+                    'N' => ("white_night", if square_color == "#666" {"darksquare"} else {"lightsquare"}),
+                    'B' => ("white_bishop", if square_color == "#666" {"darksquare"} else {"lightsquare"}),
+                    'Q' => ("white_queen", if square_color == "#666" {"darksquare"} else {"lightsquare"}),
+                    'K' => ("white_king", if square_color == "#666" {"darksquare"} else {"lightsquare"}),
+                    _   => panic!("Unknown piece"),
+                };
+
+                // // Load SVG chess piece based on piece name and background
+                let file_path = format!("pieces_svg/{}_{}.svg", piece_name, background);
+                let data_url = load_image_as_data_url(&file_path)
+                    .expect("Failed to load image as data URL");
+
+                let piece_image = Image::new()
+                    .set("x", x)
+                    .set("y", y)
+                    .set("width", 50)
+                    .set("height", 50)
+                    .set("href", data_url);
+
+                doc = doc.add(piece_image);
+            }
+        }
+    }
+
+doc
+}
+
+
+
+// Function to generate the SVG chessboard
+fn generate_white_oriented_chessboard_letters(
+    chessboard: &[[char; 8]; 8], 
+    from: Option<(usize, usize)>, 
     to: Option<(usize, usize)>
 ) -> Document {
 let mut doc = Document::new()
@@ -1802,9 +1941,6 @@ for (row, row_pieces) in chessboard.iter().enumerate() {
 
 doc
 }
-
-
-
 
 
 // Function to generate the SVG chessboard with black orientation
@@ -2085,20 +2221,6 @@ fn generate_black_oriented_chessboard_letters(
             }
         }
     }
-
-    // // Assuming `doc` is the SVG document, and `convert_svg_str_to_png` is the conversion function
-    // let svg_str = doc.to_string(); // Convert the SVG document to a string
-
-    // match convert_svg_str_to_png(&svg_str) {
-    //     Ok(png_image) => {
-    //         // Do something with the PNG image, such as saving it to a file or using it in rendering
-    //         // For example, to save it to a file:
-    //         png_image.save("output.png").expect("Failed to save PNG");
-    //         println!("SVG converted to PNG successfully");
-    //     },
-    //     Err(err) => eprintln!("Error converting SVG to PNG: {}", err),
-    // }
-
 
     doc
 }
