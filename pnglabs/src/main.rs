349304ce48bad656ecf1eb557632d9f5844f415c
lineality <lineality@gmail.com>
2023-08-10 09:18:22 -0400

    png factory overlay

--- pnglabs/src/main.rs
+++ pnglabs/src/main.rs
@@ -1,10 +1,18 @@
 extern crate image;
-
-use image::ImageBuffer;
-
 use std::path::Path;
+use image::{Rgba, ImageBuffer};
+
 
 fn combine_side_by_side<P: AsRef<Path>>(image_path1: P, image_path2: P, output_path: P) -> Result<(), image::ImageError> {
+    /*
+    extern crate image;
+    use image::ImageBuffer;
+    use std::path::Path;
+
+    combine_side_by_side("white_pawn_darksquare.png", "white_pawn_lightsquare.png", "output.png")?;
+    Ok(())
+     */
+
     // Load the images
     let image1 = image::open(image_path1)?;
     let image2 = image::open(image_path2)?;
@@ -31,7 +39,48 @@ fn combine_side_by_side<P: AsRef<Path>>(image_path1: P, image_path2: P, output_p
     Ok(())
 }
 
+
+
+fn overlay_images<P: AsRef<Path>>(image_path1: P, image_path2: P, output_path: P) -> Result<(), image::ImageError> {
+    // Load the images.
+    let mut image1 = image::open(image_path1)?.to_rgba8();
+    let image2 = image::open(image_path2)?.to_rgba8();
+
+    // Ensure the images are the same size, or handle differently as needed.
+    assert_eq!(image1.dimensions(), image2.dimensions());
+
+    // Iterate over the coordinates and pixels of image2.
+    for (x, y, pixel) in image2.enumerate_pixels() {
+        let pixel1 = image1.get_pixel_mut(x, y);
+        // Perform blending here. You could write your own blending logic or use a pre-existing function.
+        blend_pixels(pixel1, *pixel);
+    }
+
+    // Save the result.
+    image1.save(output_path)?;
+
+    Ok(())
+}
+
+// A simple alpha blending function. You might want to use a more sophisticated blending function depending on your needs.
+fn blend_pixels(bottom: &mut Rgba<u8>, top: Rgba<u8>) {
+    let alpha_top = top[3] as f32 / 255.0;
+    let alpha_bottom = bottom[3] as f32 * (1.0 - alpha_top) / 255.0;
+    let alpha_combined = alpha_top + alpha_bottom;
+
+    for i in 0..3 {
+        bottom[i] = ((top[i] as f32 * alpha_top + bottom[i] as f32 * alpha_bottom) / alpha_combined) as u8;
+    }
+    bottom[3] = (alpha_combined * 255.0) as u8;
+}
+
 fn main() -> Result<(), image::ImageError> {
-    combine_side_by_side("white_pawn_darksquare.png", "white_pawn_lightsquare.png", "output.png")?;
+
+    combine_side_by_side("white_pawn_darksquare.png", "white_pawn_lightsquare.png", "rectangle.png")?;
+
+    overlay_images("light_wood_square.png", "white_pawn_lightsquare.png", "light_overlay.png")?;
+
+    overlay_images("dark_wood_square.png", "white_pawn_darksquare.png", "dark_overlay.png")?;
+
     Ok(())
 }
--- tinyhttp_chess/src/main.rs
+++ tinyhttp_chess/src/main.rs
@@ -3967,3 +3967,40 @@ fn write_batch_to_disk(in_memory_queue: Arc<Mutex<VecDeque<Request>>>) -> Result
 
     Ok(())
 }
+
+
+fn combine_side_by_side<P: AsRef<Path>>(image_path1: P, image_path2: P, output_path: P) -> Result<(), image::ImageError> {
+    /*
+    extern crate image;
+    use image::ImageBuffer;
+    use std::path::Path;
+
+    combine_side_by_side("white_pawn_darksquare.png", "white_pawn_lightsquare.png", "output.png")?;
+    Ok(())
+     */
+
+    // Load the images
+    let image1 = image::open(image_path1)?;
+    let image2 = image::open(image_path2)?;
+
+    // Check the height of the images and make them the same if necessary, or handle differently as needed.
+    let height = std::cmp::max(image1.height(), image2.height());
+
+    // Create a new image with the combined width of both images and the maximum height
+    let mut combined_image = ImageBuffer::new(image1.width() + image2.width(), height);
+
+    // Copy pixels from image1 into the new image
+    for (x, y, pixel) in image1.to_rgba8().enumerate_pixels() {
+        combined_image.put_pixel(x, y, *pixel);
+    }
+
+    // Copy pixels from image2 into the new image, offsetting by the width of image1
+    for (x, y, pixel) in image2.to_rgba8().enumerate_pixels() {
+        combined_image.put_pixel(x + image1.width(), y, *pixel);
+    }
+
+    // Save the new image
+    combined_image.save(output_path)?;
+
+    Ok(())
+}
